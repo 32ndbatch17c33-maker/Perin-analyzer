c48743d12e62d2cf3093246bf7b54d2811b9c651
32ndbatch17c33-maker <32ndbatch17c33@gmail.com>
2026-06-22 03:02:49 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,40 @@
-const CACHE_NAME = 'perin-v1';
+const CACHE_NAME = 'perin-analyzer-v1';
 const ASSETS = [
   './',
-  './index.html'
+  './index.html',
+  './manifest.json',
+  './sw.js',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
+// Install Service Worker and Cache Assets
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching shell assets');
       return cache.addAll(ASSETS);
     })
   );
 });
 
+// Activate Service Worker and Clear Old Caches
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log('Clearing old cache');
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch Responses from Cache or Network
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((cachedResponse) => {
